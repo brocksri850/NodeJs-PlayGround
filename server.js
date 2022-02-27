b18b7689f681d674602d3773e7d21170a7f6ccf1
Sridhar <sridhar@adloggs.com>
2022-02-27 16:37:51 +0530

    server.js added

---
+++ server.js
@@ -0,0 +1,24 @@
+var str='sudharsana';
+const arr=str.split('s').join('k');
+console.log(arr);
+
+
+// var len=arr.length;
+// for(let i=0;i<=len;i++){
+//     const repall=arr.replaceAll('s','t')
+// }
+// console.log(repall);
+
+// var len =str.length;
+// const arr=[];
+// for(let i=0;i<=)
+// var splitchar=str.charAt();
+// var arr1=splitchar.replace('s','t')
+// console.log(arr1)
+
+// const arr = []
+// for(let i=0;i<len;i++){
+//     var char = str.charAt();
+//     arr1=char.arr[i];
+//     console.log(arr1)
+// }
